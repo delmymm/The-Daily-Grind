94fcfa8683a1ab3fdcc20ac6a6eccd8404c1aba8
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-12-21 21:21:52 -0700

    resolving source cgange issues

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
