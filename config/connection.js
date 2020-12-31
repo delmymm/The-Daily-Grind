0b89f2a9e52147f92b8eac733dcd4651c3617cc7
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-12-30 19:19:01 -0700

    adjusted the .env file and the connection file to add a secret phrase

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+  process.env.SECRET,
   {
     host: 'localhost',
     dialect: 'mysql',
