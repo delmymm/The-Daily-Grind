c4405fc7f55bfde373def813eead4ef12ea365f7
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-01-06 15:16:14 -0700

    fixed the mime/url issue preventing the css from working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -24,37 +24,38 @@ router.post('/', async (req, res) => {
 // Login
 router.post('/login', async (req, res) => {
   try {
-    const dbUserData = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+    return res.status(200).json({ message: 'email & password.' });
+    // const dbUserData = await User.findOne({
+    //   where: {
+    //     email: req.body.email,
+    //   }
+    // });
+ 
+    // if (!dbUserData) {
+    //   res
+    //     .status(400)
+    //     .json({ message: 'Incorrect email or password. Please try again!' });
+    //   return;
+    // }
 
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
+    // const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
+    // if (!validPassword) {
+    //   res
+    //     .status(400)
+    //     .json({ message: 'Incorrect email or password. Please try again!' });
+    //   return;
+    // }
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
+    // req.session.save(() => {
+    //   req.session.loggedIn = true;
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
-
-      req.session.user_id = dbUserData.id;
-      console.log(req.session);
-      res.redirect("/");
-        // .status(200)
-        // .json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+    //   req.session.user_id = dbUserData.id;
+    //   console.log(req.session);
+    //   res.redirect("/homepage");
+    //     // .status(200)
+    //     // .json({ user: dbUserData, message: 'You are now logged in!' });
+    // });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
